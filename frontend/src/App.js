038b0e983b70b47ca3f05c069f0f521aa0e3be2a
paulloy <paulloy020896@gmail.com>
2021-10-12 20:06:22 +0100

    Add GET request for ink

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,71 +1,110 @@
-import './index.css';
-import React, { useEffect, useRef, useState } from 'react';
+import "./index.css";
+import React, { useEffect, useRef, useState } from "react";
 
-import paperData from './models/test_data/test_paper_data.json';
-import { Paper } from './models/paper.model';
-import axios from 'axios';
+import paperData from "./models/test_data/test_paper_data.json";
+import { Paper } from "./models/paper.model";
+import axios from "axios";
 
-import PaperForm from './components/PaperForm';
-import PricingForm from './components/PricingForm';
+import PaperForm from "./components/PaperForm";
+import PricingForm from "./components/PricingForm";
 
-function App() {    
+import { getPaper } from "./requests/paperRequests";
 
-  const [paper, setPaper] = useState(paperData);
+function App() {
+  const [paper, setPaper] = useState({
+    isLoaded: false,
+    data: [],
+  });
+
+  const [inkCost, setInkCost] = useState(0);
 
   useEffect(() => {
-    axios.get('http://localhost:4000/api/paper')
-    .then(res => 
-      setPaper(res.data.map((data) => {
-        return new Paper(data._id, data.name, data.length, data.width, data.cost);
-      }))
-    )
-  }, []); 
+    axios.get("http://localhost:4000/api/paper").then((res) =>
+      setPaper({
+        isLoaded: true,
+        data: res.data.map((data) => {
+          return new Paper(
+            data._id,
+            data.name,
+            data.length,
+            data.width,
+            data.cost
+          );
+        }),
+      })
+    );
+    
+    axios.get("http://localhost:4000/api/ink").then((res) =>
+      setInkCost(res.data)
+    );  
+  }, []);
   
   const handleDelete = (id) => {
-    axios.delete(`http://localhost:4000/api/paper/delete/${id}`)
-    .then(res => 
-      console.log(res)
-      );
-    }
-    
+    axios
+      .delete(`http://localhost:4000/api/paper/delete/${id}`)
+      .then((res) => console.log(res));
+  };
+  
   // do this because typescript
-  if (paper === paperData) return <p>Loading Data</p>;
-
+  if (!paper) return <p>Loading Data</p>;
+  
   return (
     <React.Fragment>
-      {/* <PaperForm />
+      <PricingForm paper={paper.data} inkCost={inkCost} />
+      <PaperForm />
       <table>
         <thead>
           <tr>
-            <td><strong>Paper Name</strong></td>
-            <td><strong>Length (cm)</strong></td>
-            <td><strong>Width (cm)</strong></td>
-            <td><strong>Paper Cost (£)</strong></td>
-            <td><strong>Paper Cost per cm</strong></td>
-            <td><strong>Length (inches)</strong></td>
-            <td><strong>Width (inches)</strong></td>
-            <td><strong>Paper Cost per Inch</strong></td>
-            <td><strong>DELETE</strong></td>
+            <td>
+              <strong>Paper Name</strong>
+            </td>
+            <td>
+              <strong>Length (cm)</strong>
+            </td>
+            <td>
+              <strong>Width (cm)</strong>
+            </td>
+            <td>
+              <strong>Paper Cost (£)</strong>
+            </td>
+            <td>
+              <strong>Paper Cost per cm</strong>
+            </td>
+            <td>
+              <strong>Length (inches)</strong>
+            </td>
+            <td>
+              <strong>Width (inches)</strong>
+            </td>
+            <td>
+              <strong>Paper Cost per Inch</strong>
+            </td>
+            <td>
+              <strong>DELETE</strong>
+            </td>
           </tr>
         </thead>
         <tbody>
-        { paper.map((obj) => (
-          <tr key={ obj.id }>
-            <td>{ obj.name }</td>
-            <td>{ parseFloat(obj.length_cm).toFixed(2) } cm</td>
-            <td>{ parseFloat(obj.width_cm).toFixed(2) } cm</td>
-            <td>£{ obj.cost }</td>
-            <td>{ parseFloat(obj.costPerUnitArea_cm).toFixed(2) } £/cm</td>
-            <td>{ parseFloat(obj.length_inch).toFixed(2) } inch</td>
-            <td>{ parseFloat(obj.width_inch).toFixed(2) } inch</td>
-            <td>{ parseFloat(obj.costPerUnitArea_inch).toFixed(2) } £/inch</td>
-            <td><button type="button" onClick={() => handleDelete(obj.id)}>DELETE</button></td>
-          </tr>
-        )) }
+          {paper.data.map((obj) => (
+            <tr key={obj.id}>
+              <td>{obj.name}</td>
+              <td>{parseFloat(obj.length_cm).toFixed(2)} cm</td>
+              <td>{parseFloat(obj.width_cm).toFixed(2)} cm</td>
+              <td>£{obj.cost}</td>
+              <td>{parseFloat(obj.costPerUnitArea_cm).toFixed(2)} £/cm</td>
+              <td>{parseFloat(obj.length_inch).toFixed(2)} inch</td>
+              <td>{parseFloat(obj.width_inch).toFixed(2)} inch</td>
+              <td>{parseFloat(obj.costPerUnitArea_inch).toFixed(2)} £/inch</td>
+              <td>
+                <button type="button" onClick={() => handleDelete(obj.id)}>
+                  DELETE
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-      </table> 
-           */}
-      <PricingForm paper={paper} />
+      </table>
+
     </React.Fragment>
   );
 }
