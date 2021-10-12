c79222ae3bbc500367129ce915485aaf2173e517
paulloy <paulloy020896@gmail.com>
2021-10-12 20:06:50 +0100

    Add new property to ink model

---
+++ express-mongodb/models/ink.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create student schema & model
+const InkSchema = new Schema({
+    cost: {
+        type: Number,
+    },
+    volume: {
+        type: Number
+    }
+});
+
+
+const Ink = mongoose.model('ink', InkSchema);
+
+module.exports = Ink;
