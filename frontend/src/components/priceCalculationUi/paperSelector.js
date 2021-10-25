2205acda037bd9c8a6d54a06b345ace78fc052eb
paulloy <paulloy020896@gmail.com>
2021-10-25 20:09:04 +0100

    paperSelector is now a nested component

---
+++ frontend/src/components/priceCalculationUi/paperSelector.js
@@ -0,0 +1,23 @@
+export default function PaperSelector({ papers, selectedPaper, getSelectedPaper = f => f }) {
+
+    return (
+        <div className="col-3-custom p-4 glass-morphism">
+            <span className="d-flex justify-content-between align-items-center border-b-double mb-3">
+                <h4 className="align-left m-0">Step 2 - Your Paper</h4>
+                <button className="settings"><i class="fas fa-cog"></i></button>
+            </span>
+            <select 
+                className="form-select form-select-lg mb-3" 
+                name="paper-selector" 
+                id="paper-selector" 
+                onChange={(e) => getSelectedPaper(e)}>
+                    <option className="d-none" value="not a value">Please select paper</option>
+                    {papers.map(paper => (
+                        <option value={paper.name}>{paper.name}</option>
+                    ))}
+            </select>
+            <img className="img" src="https://www.blueskyprinting.co.uk/app/uploads/2021/02/giclee-fine-art-print-japan.jpg" alt="temp foto" />
+            <p className="p-2 sans-serif-custom">{selectedPaper ? selectedPaper.description : null}</p>
+        </div>
+    );
+}
--- frontend/src/components/priceCalculationUi/priceCalculationUi.js
+++ frontend/src/components/priceCalculationUi/priceCalculationUi.js
@@ -2,37 +2,13 @@ import { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import './price-calculation-ui.css';
 import ImageDimensions from './imageDimensions';
-
-// Paper model
-class Paper {
-    // Axios request data
-    id;
-    name;
-    width;
-    length;
-    rollCost;
-    description;
-    // 
-    area;
-    paperCostPerUnitArea;
-
-    constructor(id=id, name=name, width=width, length=length, rollCost=rollCost, description=description) {
-        this.id = id;
-        this.name = name;
-        this.width = width;
-        this.length = length;
-        this.rollCost = rollCost;
-        this.description = description;
-        this.area = width * length;
-        this.paperCostPerUnitArea = rollCost / (width * length);
-    }
-}
+import Paper from './../../models/paper.model';
+import PaperSelector from './paperSelector';
 
 export const DimensionsContext = createContext();
 
 export default function PriceCalculationUi() {
 
-    // STATE
     const [dimensions, setDimensions] = useState({
         unit: 'inches',
         width: 0,
@@ -72,12 +48,6 @@ export default function PriceCalculationUi() {
         });
     }
 
-    const getSelectedPaper = (event) => {
-        setSelectedPaper(papers.filter(paper => {
-            return paper.name === event.target.value
-        })[0]);
-    }
-
     useEffect(() => {
         calculateTotal();
     }, [dimensions, selectedPaper]);
@@ -91,24 +61,11 @@ export default function PriceCalculationUi() {
                         <ImageDimensions/>
                     </DimensionsContext.Provider>
 
-                    <div className="col-3-custom p-4 glass-morphism">
-                        <span className="d-flex justify-content-between align-items-center border-b-double mb-3">
-                            <h4 className="align-left m-0">Step 2 - Your Paper</h4>
-                            <button className="settings"><i class="fas fa-cog"></i></button>
-                        </span>
-                        <select 
-                            className="form-select form-select-lg mb-3" 
-                            name="paper-selector" 
-                            id="paper-selector" 
-                            onChange={(e) => getSelectedPaper(e)}>
-                                <option className="d-none" value="not a value">Please select paper</option>
-                                {papers.map(paper => (
-                                    <option value={paper.name}>{paper.name}</option>
-                                ))}
-                        </select>
-                        <img className="img" src="https://www.blueskyprinting.co.uk/app/uploads/2021/02/giclee-fine-art-print-japan.jpg" alt="temp foto" />
-                        <p className="p-2 sans-serif-custom">{selectedPaper ? selectedPaper.description : null}</p>
-                    </div>
+                    <PaperSelector 
+                        papers={papers} 
+                        selectedPaper={selectedPaper} 
+                        getSelectedPaper={(e) => setSelectedPaper(papers.filter(paper => paper.name === e.target.value)[0])}/>
+
                     <div className="col-3-custom p-4 glass-morphism">
                         <span className="d-flex justify-content-between align-items-center border-b-double mb-3 p-0">
                             <h4 className="align-left m-0">Order Details</h4>
@@ -144,42 +101,6 @@ export default function PriceCalculationUi() {
                         </div>
                     </div>
                 </span>
-                {/* Cart */}
-                {/* <div className="col-12 d-flex flex-column p-4 glass-morphism">
-                    <h4 className="align-left">Order Summary</h4>
-                    A carousel containing uploaded imgs, or imgs from cart?
-                    <div className="row">
-                        <div className="col-6"><strong>Paper Type</strong></div>
-                        <div className="col-6">Photo Lustre</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6"><strong>Width</strong></div>
-                        <div className="col-6">8 inches</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6"><strong>Length</strong></div>
-                        <div className="col-6">10 inches</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6"><strong>Quantity</strong></div>
-                        <div className="col-6">1</div>
-                    </div>
-                    <hr />
-                    <div className="row">
-                        <div className="col-6"><strong>Sub-total</strong></div>
-                        <div className="col-6">£</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6"><strong>VAT</strong></div>
-                        <div className="col-6">£0.00</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6"><strong>Grand Total</strong></div>
-                        <div className="col-6">£0.00</div>
-                    </div>
-                    <hr />
-                    <button className="btn btn-primary">Add to Cart</button>
-                </div> */}
             </div>
         </div>
     );
